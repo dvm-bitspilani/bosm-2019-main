2dc59e339e22df0e8f3218c1188fd8f8388b0ec2
chirag-singhal <csinghal208@gmail.com>
2019-08-12 17:23:34 +0530

    gallery

---
+++ js/gallery.js
@@ -0,0 +1,27 @@
+let galleryNumber = 1;
+
+const galleryContainer = document.getElementsByClassName("gallery-container")[0];
+
+const changeGallerySet = () => {
+    console.log(galleryNumber)
+    if (galleryNumber < numberOfSet) {
+        galleryNumber++;
+        galleryContainer.style.transform = "translate(" + (-100 * (galleryNumber - 1)) + "%)";
+        for (dot of document.getElementsByClassName('gallery-nav-dot')) {
+                dot.style.background = 'none';
+        }
+        document.getElementsByClassName('gallery-nav-dot')[galleryNumber - 1].style.background = "#ffffff";
+    } else {
+        galleryNumber = 0;
+        changeGallerySet();
+    }
+}
+
+let gallerySetChangeInterval = setInterval(changeGallerySet, 5000);
+
+const navigateGallery = (dotIndex) => {
+    galleryNumber = dotIndex - 1;
+    changeGallerySet();
+    clearInterval(gallerySetChangeInterval);
+    gallerySetChangeInterval = setInterval(changeGallerySet, 5000);
+}
