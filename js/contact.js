afe212151bccc625e723fd2ce9387ce4aa567c03
Akshit-Patel <akshitpatel72@gmail.com>
2019-08-15 01:56:34 +0530

    contact us new version

--- js/contact.js
+++ js/contact.js
@@ -1,41 +1,60 @@
-// var noOfContacts = 8;
-// var inView = 4;
-// var widthOfContact = document.getElementsByClassName('cards-container')[0].offsetWidth;
-// var currentGroup = 1;
-// var noOfGroups= 2;
-// function setLayout(){
-//     widthOfContact = document.getElementsByClassName('cards-container')[0].offsetWidth;
-//     var cards = document.getElementsByClassName('cards')[0].offsetWidth;
-//     var inView = Math.floor(widthOfContact/(2*cards));
-//     console.log(inView);
-// }
+var arrow = document.getElementsByClassName('arrow')[0];
+var isOpen = false;
+var instructions = document.getElementsByClassName('instructions')[0];
+var h1 = document.querySelectorAll('.instructions h1')[0];
+var h2 = document.querySelectorAll('.instructions div')[0]; 
+var cardsContainer = document.getElementsByClassName('cards-container')[0];
+console.log("h1"+h1);
+console.log("h2"+h2);
+console.log("card "+cardsContainer);
+function translate(action){
+    var container= document.getElementsByClassName('contact-main-container')[0];
+    if(action){
+        container.style.transform="translate(-50vw)";
+        setTimeout(()=>{
+            h1.style.opacity = 0;
+        },750);
+        
+        setTimeout(()=>{
+            h2.style.opacity = 0;
+        },800);
 
-// function navigate(id){
-//     // currentGroup++;
-//     var cardsContainer = document.getElementsByClassName('cards-container')[0];
-//     var dots = document.getElementsByClassName('contact-nav-dot');
-//     if( id == 2){   
-//         cardsContainer.style.transform="translate(-800px)";
-//         dots[id-1].style.background = "white";
-//         dots[0].style.background = "none";
-//     }else{
-//         cardsContainer.style.transform="translate(0px)";
-//         dots[0].style.background = "white"; 
-//         dots[1].style.background = "none";        
-//     }
-// }
+        setTimeout(()=>{
+           instructions.style.opacity = 0;
+           cardsContainer.style.opacity = 1; 
+           arrow.style.transform = "rotateY(180deg) translateX(-60px)";         
+        },1000);
 
-// var counter = 1;
-// setInterval(() => {
-//     counter++;
-//     if(counter ==2){
-//         navigate(counter);
-//     }else{
-//         counter=1;
-//         navigate(counter);
-//     }
 
-// },3000);
-// navigate(1);
-// setLayout();
+    }else{
+        console.log("hi");
+        h1.style.opacity = 1;
+        h2.style.opacity = 1;
+        instructions.style.opacity = 1;
+        cardsContainer.style.opacity = 0;   
+        container.style.transform="translate(0vw)";
 
+        setTimeout(()=>{
+           arrow.style.transform = "rotateY(0deg) translateX(0px)";                     
+        },750);
+        
+        // setTimeout(()=>{
+        //     h2.style.opacity = 1;
+        // },1000);
+
+        // setTimeout(()=>{
+        //    instructions.style.opacity = 1;
+        //    cardsContainer.style.opacity = 0;           
+        // },750);  
+    }
+}
+
+arrow.addEventListener('click',()=>{
+    if(!isOpen){
+        translate(!isOpen);
+        isOpen = true;
+    }else{
+        translate(!isOpen);
+        isOpen = false;        
+    }
+});
