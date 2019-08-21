cc12dfe8372217d09792830bccee507efe95010d
chirag-singhal <csinghal208@gmail.com>
2019-08-21 23:03:29 +0530

    video slider paused

--- js/about.js
+++ js/about.js
@@ -166,7 +166,7 @@ function ytpage(i, e) {
 
 // 2. This code loads the IFrame Player API code asynchronously.
 var tag = document.createElement('script');
-tag.src = "http://www.youtube.com/iframe_api";
+tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
